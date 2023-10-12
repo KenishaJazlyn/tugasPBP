4e880f87ad0644bcd6b52872b4bcd5661e8de135
kenishajazlyn <124899946+KenishaJazlyn@users.noreply.github.com>
2023-10-13 02:00:10 +0700

    TUGAS 6

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main.views import show_main
 from main.views import show_main, add_product, show_xml, show_xml_by_id, show_json_by_id, show_json, register,  login_user, logout_user
-from main.views import increment_quantity, decrement_quantity, delete_item
+from main.views import increment_quantity, decrement_quantity, delete_item, add_product_ajax, get_product_json
 
 
 app_name = 'main'
@@ -14,6 +14,7 @@ urlpatterns = [
     ,path('logout/', logout_user, name='logout'),path('login/', login_user, name='login'), 
     path('increment_quantity/<int:product_id>/', increment_quantity, name='increment_quantity'),
     path('decrement_quantity/<int:product_id>/', decrement_quantity, name='decrement_quantity'),
-    path('delete_item/<int:product_id>/', delete_item, name='delete_item')
-
+    path('delete_item/<int:product_id>/', delete_item, name='delete_item'),
+    path('get-product/', get_product_json, name='get_product_json'),
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax'),
 ]
